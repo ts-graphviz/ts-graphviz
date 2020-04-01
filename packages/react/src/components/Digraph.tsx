05a4c7c561fe98be59eefd4be3dedaeeb5273bb1
kamiazya <yuki@kamiazya.tech>
2020-04-01 21:15:08 +0900

    use memo

--- packages/react/src/components/Digraph.tsx
+++ packages/react/src/components/Digraph.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import * as gv from 'ts-graphviz';
 import { ClusterContext } from '../contexts/ClusterContext';
 import { useGraphvizContext } from '../hooks/useContext';
 
 export const Digraph: FC = ({ children }) => {
   const context = useGraphvizContext();
-  const g = new gv.Digraph(context);
+  const g = useMemo(() => new gv.Digraph(context), [context]);
   return <ClusterContext.Provider value={g}>{children}</ClusterContext.Provider>;
 };
