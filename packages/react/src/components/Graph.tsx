765241507f24b134deaaaf4fecaa9e1b19186811
kamiazya <yuki@kamiazya.tech>
2020-04-01 21:15:08 +0900

    use memo

--- packages/react/src/components/Graph.tsx
+++ packages/react/src/components/Graph.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import gv from 'ts-graphviz';
 import { ClusterContext } from '../contexts/ClusterContext';
 import { useGraphvizContext } from '../hooks/useContext';
 
 export const Graph: FC = ({ children }) => {
   const context = useGraphvizContext();
-  const g = new gv.Graph(context);
+  const g = useMemo(() => new gv.Graph(context), [context]);
   return <ClusterContext.Provider value={g}>{children}</ClusterContext.Provider>;
 };
--- packages/react/src/components/Node.tsx
+++ packages/react/src/components/Node.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { NodeContext } from '../contexts/NodeContext';
 import { useCluster } from '../hooks/useCluster';
 export const Node: FC<{ id: string }> = ({ children, id }) => {
   const cluster = useCluster();
-  const node = cluster?.createNode(id);
+  const node = useMemo(() => cluster?.createNode(id), [cluster, id]);
   return <NodeContext.Provider value={node}>{children}</NodeContext.Provider>;
 };
