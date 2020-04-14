a72e8f995d8c807c149e8df2a1ca1e6b54082bd0
kamiazya <yuki@kamiazya.tech>
2020-04-14 22:50:14 +0900

    fix component effects

--- packages/react/src/components/Subgraph.tsx
+++ packages/react/src/components/Subgraph.tsx
@@ -1,10 +1,15 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 import { ClusterContext } from '../contexts/ClusterContext';
 import { useCluster } from '../hooks/useCluster';
 
 export const Subgraph: FC<{ id?: string }> = ({ children, id }) => {
   const cluster = useCluster();
-  const subgraph = cluster.createSubgraph(id);
+  const subgraph = useMemo(() => cluster.createSubgraph(id), [cluster, id]);
+  useEffect(() => {
+    return () => {
+      cluster.removeSubgraph(subgraph);
+    };
+  }, [cluster, subgraph]);
   return <ClusterContext.Provider value={subgraph}>{children}</ClusterContext.Provider>;
 };
 
