f4359fb4170ca6a78f5a47556f3ef6991407ba10
kamiazya <yuki@kamiazya.tech>
2020-04-15 00:27:48 +0900

    upgrade ts-graphviz

--- packages/react/src/components/Node.tsx
+++ packages/react/src/components/Node.tsx
@@ -1,12 +1,25 @@
 import React, { forwardRef, useEffect, useImperativeHandle, useMemo } from 'react';
-import { INode } from 'ts-graphviz';
+import { INode, NodeAttributes } from 'ts-graphviz';
 import { NodeContext } from '../contexts/NodeContext';
 import { useCluster } from '../hooks/useCluster';
 
-export const Node = forwardRef<INode, { id: string }>(({ children, id }, ref) => {
+type Props = {
+  id: string;
+  comment?: string;
+} & NodeAttributes;
+
+export const Node = forwardRef<INode, Props>(({ children, id, comment, ...attributes }, ref) => {
   const cluster = useCluster();
   const node = useMemo(() => cluster.createNode(id), [cluster, id]);
   useImperativeHandle(ref, () => node, [node]);
+  useEffect(() => {
+    node.attributes.clear();
+    node.attributes.apply(attributes);
+  }, [node, attributes]);
+
+  useEffect(() => {
+    node.comment = comment;
+  }, [node, comment]);
   useEffect(() => {
     return () => {
       cluster.removeNode(node);
