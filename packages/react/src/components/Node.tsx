4ee771de6f28ab0e668e77b50228315b824cbf1d
kamiazya <yuki@kamiazya.tech>
2020-04-14 22:50:14 +0900

    fix component effects

--- packages/react/src/components/Node.tsx
+++ packages/react/src/components/Node.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useImperativeHandle, useMemo } from 'react';
+import React, { forwardRef, useEffect, useImperativeHandle, useMemo } from 'react';
 import { INode } from 'ts-graphviz';
 import { NodeContext } from '../contexts/NodeContext';
 import { useCluster } from '../hooks/useCluster';
@@ -7,6 +7,11 @@ export const Node = forwardRef<INode, { id: string }>(({ children, id }, ref) =>
   const cluster = useCluster();
   const node = useMemo(() => cluster.createNode(id), [cluster, id]);
   useImperativeHandle(ref, () => node, [node]);
+  useEffect(() => {
+    return () => {
+      cluster.removeNode(node);
+    };
+  }, [cluster, node]);
   return <NodeContext.Provider value={node}>{children}</NodeContext.Provider>;
 });
 
