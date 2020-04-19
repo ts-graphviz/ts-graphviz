7d8b32da60db11f334d4377bd467bd94a326da0b
kamiazya <yuki@kamiazya.tech>
2020-04-19 20:21:58 +0900

    clean reconciler

--- packages/react/src/renderer/reconciler.ts
+++ packages/react/src/renderer/reconciler.ts
@@ -4,12 +4,12 @@
 import { FC } from 'react';
 import ReactReconciler from 'react-reconciler';
 
-type Type = FC; // 'Digraph' | 'Graph' | 'Node' | 'Edge' | 'Subgraph';
+type Type = FC;
 type Props = any;
 type Container = {};
 
-type Instance = any; // gv.Digraph | gv.Graph | gv.Node | gv.Edge | gv.Subgraph;
-type TextInstance = any; // gv.LblStringValue;
+type Instance = any;
+type TextInstance = any;
 type HydratableInstance = any;
 type PublicInstance = any;
 type HostContext = any;
@@ -55,17 +55,14 @@ export class HostConfig
 
   public supportsHydration = false;
 
-  // @Debug
   public getPublicInstance(instance: Instance | TextInstance): PublicInstance {
     return instance;
   }
 
-  // @Debug
   public getRootHostContext(rootContainerInstance: Container): HostContext {
     return {};
   }
 
-  // @Debug
   public getChildHostContext(
     parentHostContext: HostContext,
     type: Type,
@@ -74,12 +71,10 @@ export class HostConfig
     return parentHostContext;
   }
 
-  // @Debug
   public prepareForCommit(containerInfo: Container): void {
     // NoOp
   }
 
-  // @Debug
   public resetAfterCommit(containerInfo: Container): void {
     // containerInfo.setRoot
   }
@@ -87,7 +82,6 @@ export class HostConfig
   /**
    * Create component instance
    */
-  // @Debug
   public createInstance(
     type: Type,
     props: Props,
@@ -99,12 +93,10 @@ export class HostConfig
     return type(props);
   }
 
-  // @Debug
   public appendInitialChild(parentInstance: Instance, child: Instance | TextInstance): void {
     parentInstance.appendChild(child);
   }
 
-  // @Debug
   public finalizeInitialChildren(
     parentInstance: Instance,
     type: Type,
@@ -115,7 +107,6 @@ export class HostConfig
     return false;
   }
 
-  // @Debug
   public prepareUpdate(
     instance: Instance,
     type: Type,
@@ -127,17 +118,14 @@ export class HostConfig
     return {};
   }
 
-  // @Debug
   public shouldSetTextContent(type: Type, props: Props): boolean {
     return false;
   }
 
-  // @Debug
   public shouldDeprioritizeSubtree(type: Type, props: Props): boolean {
     return false;
   }
 
-  // @Debug
   public createTextInstance(
     text: string,
     rootContainerInstance: Container,
@@ -147,12 +135,10 @@ export class HostConfig
     return text;
   }
 
-  // @Debug
   public scheduleDeferredCallback(callback: () => any, options?: { timeout: number }): any {
     // NoOp
   }
 
-  // @Debug
   public cancelDeferredCallback(callbackID: any): void {
     // NoOp
   }
@@ -161,7 +147,6 @@ export class HostConfig
   //      Mutation
   //     (optional)
   // -------------------
-  // @Debug
   public appendChild(parentInstance: Instance, child: Instance | TextInstance): void {
     // NoOp
     if (parentInstance.appendChild) {
@@ -169,24 +154,20 @@ export class HostConfig
     }
   }
 
-  // @Debug
   public appendChildToContainer(container: Container, child: Instance | TextInstance): void {
     // if (container.appendChild) {
     //   container.appendChild(child);
     // }
   }
 
-  // @Debug
   public commitTextUpdate(textInstance: TextInstance, oldText: string, newText: string): void {
     // NoOp
   }
 
-  // @Debug
   public commitMount(instance: Instance, type: Type, newProps: Props, internalInstanceHandle: OpaqueHandle): void {
     // NoOp
   }
 
-  // @Debug
   public commitUpdate(
     instance: Instance,
     updatePayload: UpdatePayload,
@@ -198,7 +179,6 @@ export class HostConfig
     // NoOp
   }
 
-  // @Debug
   public insertBefore(
     parentInstance: Instance,
     child: Instance | TextInstance,
@@ -207,7 +187,6 @@ export class HostConfig
     // NoOp
   }
 
-  // @Debug
   public insertInContainerBefore(
     container: Container,
     child: Instance | TextInstance,
@@ -216,107 +195,17 @@ export class HostConfig
     // NoOp
   }
 
-  // @Debug
   public removeChild(parentInstance: Instance, child: Instance | TextInstance): void {
     // NoOp
   }
 
-  // @Debug
   public removeChildFromContainer(container: Container, child: Instance | TextInstance): void {
     // NoOp
   }
 
-  // @Debug
   public resetTextContent(instance: Instance): void {
     // NoOp
   }
-
-  // -------------------
-  //     Persistence
-  //     (optional)
-  // -------------------
-  // export declare function cloneInstance(
-  //   instance: Instance,
-  //   updatePayload: null | UpdatePayload,
-  //   type: Type,
-  //   oldProps: Props,
-  //   newProps: Props,
-  //   internalInstanceHandle: OpaqueHandle,
-  //   keepChildren: boolean,
-  //   recyclableInstance: Instance,
-  // ): Instance;
-
-  // export declare function createContainerChildSet(container: Container): ChildSet;
-
-  // export declare function appendChildToContainerChildSet(childSet: ChildSet, child: Instance | TextInstance): void;
-  // export declare function finalizeContainerChildren(container: Container, newChildren: ChildSet): void;
-
-  // export declare function replaceContainerChildren(container: Container, newChildren: ChildSet): void;
-
-  // -------------------
-  //     Hydration
-  //     (optional)
-  // -------------------
-  // export declare function canHydrateInstance(instance: HydratableInstance, type: Type, props: Props): null | Instance;
-  // export declare function canHydrateTextInstance(instance: HydratableInstance, text: string): null | TextInstance;
-  // export declare function getNextHydratableSibling(
-  //   instance: Instance | TextInstance | HydratableInstance,
-  // ): null | HydratableInstance;
-  // export declare function getFirstHydratableChild(parentInstance: Instance | Container): null | HydratableInstance;
-  // export declare function hydrateInstance(
-  //   instance: Instance,
-  //   type: Type,
-  //   props: Props,
-  //   rootContainerInstance: Container,
-  //   hostContext: HostContext,
-  //   internalInstanceHandle: OpaqueHandle,
-  // ): null | UpdatePayload;
-  // export declare function hydrateTextInstance(
-  //   textInstance: TextInstance,
-  //   text: string,
-  //   internalInstanceHandle: OpaqueHandle,
-  // ): boolean;
-  // export declare function didNotMatchHydratedContainerTextInstance(
-  //   parentContainer: Container,
-  //   textInstance: TextInstance,
-  //   text: string,
-  // ): void;
-  // export declare function didNotMatchHydratedTextInstance(
-  //   parentType: Type,
-  //   parentProps: Props,
-  //   parentInstance: Instance,
-  //   textInstance: TextInstance,
-  //   text: string,
-  // ): void;
-  // export declare function didNotHydrateContainerInstance(
-  //   parentContainer: Container,
-  //   instance: Instance | TextInstance,
-  // ): void;
-  // export declare function didNotHydrateInstance(
-  //   parentType: Type,
-  //   parentProps: Props,
-  //   parentInstance: Instance,
-  //   instance: Instance | TextInstance,
-  // ): void;
-  // export declare function didNotFindHydratableContainerInstance(
-  //   parentContainer: Container,
-  //   type: Type,
-  //   props: Props,
-  // ): void;
-  // export declare function didNotFindHydratableContainerTextInstance(parentContainer: Container, text: string): void;
-  // export declare function didNotFindHydratableInstance(
-  //   parentType: Type,
-  //   parentProps: Props,
-  //   parentInstance: Instance,
-  //   type: Type,
-  //   props: Props,
-  // ): void;
-  // export declare function didNotFindHydratableTextInstance(
-  //   parentType: Type,
-  //   parentProps: Props,
-  //   parentInstance: Instance,
-  //   text: string,
-  // ): void;
 }
 
 export const reconciler = ReactReconciler(new HostConfig());
