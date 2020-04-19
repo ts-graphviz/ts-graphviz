f6b821288276fcd5b78259cc1c39e338204b2ac1
kamiazya <yuki@kamiazya.tech>
2020-04-19 17:04:14 +0900

    add useRenderedID hook

--- packages/react/src/components/Edge.tsx
+++ packages/react/src/components/Edge.tsx
@@ -1,14 +1,14 @@
-import React, { FC, ReactElement, useMemo } from 'react';
+import React, { FC, ReactElement } from 'react';
 import PropTypes from 'prop-types';
 import { useEdge, EdgeProps } from '../hooks/use-edge';
-import { renderId } from '../utils/renderId';
+import { useRenderedID } from '../hooks/use-rendered-id';
 
 type Props = Omit<EdgeProps, 'label'> & {
   label?: ReactElement | string;
 };
 
 export const Edge: FC<Props> = ({ children, label, ...props }) => {
-  const renderedLabel = useMemo(() => renderId(label), [label]);
+  const renderedLabel = useRenderedID(label);
   if (renderedLabel !== undefined) Object.assign(props, { label: renderedLabel });
   useEdge(props);
   return <>{children}</>;
