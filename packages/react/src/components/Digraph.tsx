004a2d9db865c80936f12f4b45243f8873bd357e
kamiazya <yuki@kamiazya.tech>
2020-04-19 14:04:22 +0900

    add hooks

--- packages/react/src/components/Digraph.tsx
+++ packages/react/src/components/Digraph.tsx
@@ -1,20 +1,60 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useCallback, useEffect, ReactElement } from 'react';
 import PropTypes from 'prop-types';
 import * as gv from 'ts-graphviz';
 import { RootClusterContext } from '../contexts/RootClusterContext';
 import { ClusterContext } from '../contexts/ClusterContext';
 import { useGraphvizContext } from '../hooks/useContext';
+import { renderId } from '../utils/renderId';
+
+type ReactRootClusterAttributes = Omit<gv.RootClusterAttributes, 'label'> & {
+  label?: ReactElement | string;
+};
 
 type Props = {
   id?: string;
+  comment?: string;
+} & ReactRootClusterAttributes;
+
+const createDigraph = ({ id, comment, ...attributes }: Props): { digraph: gv.Digraph } => {
+  const context = useGraphvizContext();
+  const apply = useCallback((g: gv.IRootCluster, a: ReactRootClusterAttributes, clear = false) => {
+    if (clear) {
+      g.clear();
+    }
+    const { label, ...attrs } = a;
+    if (label) {
+      Object.assign(attrs, { label: renderId(label) });
+    }
+    g.apply(attrs);
+  }, []);
+
+  const digraph = useMemo(() => {
+    const g = new gv.Digraph(context, id);
+    g.comment = comment;
+    apply(g, attributes);
+    return g;
+  }, [context, id, comment, attributes]);
+  useEffect(() => {
+    return (): void => {
+      context.root = undefined;
+    };
+  }, [context]);
+
+  useEffect(() => {
+    apply(digraph, attributes, true);
+  }, [digraph, attributes]);
+
+  useEffect(() => {
+    digraph.comment = comment;
+  }, [digraph, comment]);
+  return { digraph };
 };
 
-export const Digraph: FC<Props> = ({ children, id }) => {
-  const context = useGraphvizContext();
-  const g = useMemo(() => new gv.Digraph(context, id), [context, id]);
+export const Digraph: FC<Props> = ({ children, ...props }) => {
+  const { digraph } = createDigraph(props);
   return (
-    <RootClusterContext.Provider value={g}>
-      <ClusterContext.Provider value={g}>{children}</ClusterContext.Provider>
+    <RootClusterContext.Provider value={digraph}>
+      <ClusterContext.Provider value={digraph}>{children}</ClusterContext.Provider>
     </RootClusterContext.Provider>
   );
 };
@@ -23,8 +63,10 @@ Digraph.displayName = 'Digraph';
 
 Digraph.defaultProps = {
   id: undefined,
+  comment: undefined,
 };
 
 Digraph.propTypes = {
   id: PropTypes.string,
+  comment: PropTypes.string,
 };
