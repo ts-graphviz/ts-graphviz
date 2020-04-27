b7b4171d03ad0ee22a1ffcf8a7ec136dfa8a5879
kamiazya <yuki@kamiazya.tech>
2020-04-27 21:53:39 +0900

    fix html label to upper case

--- packages/react/src/utils/render-id.ts
+++ packages/react/src/utils/render-id.ts
@@ -2,14 +2,11 @@ import { ReactElement, isValidElement } from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 
 export function renderId(id?: ReactElement | string): string | undefined {
-  if (typeof id === 'string') {
-    return id;
-  }
   if (isValidElement(id)) {
     const htmlLike = renderToStaticMarkup(id)
       .replace(/<dot-port>(.+?)<\/dot-port>/gi, '<$1>')
-      .replace(/<(\/?)dot-/gi, '<$1');
+      .replace(/<(\/?)dot-([a-z-]+)/gi, (_, $1, $2) => `<${$1}${$2.toUpperCase()}`);
     return `<${htmlLike}>`;
   }
-  return undefined;
+  return id;
 }
