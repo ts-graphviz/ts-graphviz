b728b18824d8e62b3756ee3997cd126fe4846413
kamiazya <yuki@kamiazya.tech>
2020-05-05 21:38:58 +0900

    add test for Graphviz component

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   verbose: true,
   collectCoverage: true,
   setupFilesAfterEnv: ['<rootDir>/config/jest/setup-jest.ts'],
