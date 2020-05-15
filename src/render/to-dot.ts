855883c5605aceff033bf50d51d58d49c44b5949
kamiazya <yuki@kamiazya.tech>
2020-05-15 23:25:18 +0900

    fix

--- src/render/to-dot.ts
+++ src/render/to-dot.ts
@@ -2,7 +2,7 @@
 import { Renderer } from './renderer';
 import { IDotContext, Dot } from '../types';
 
-export function toDot(object: Dot, context?: IDotContext): string | undefined {
+export function toDot(object: Dot, context?: IDotContext): string {
   const renderer = new Renderer(context);
   return renderer.render(object);
 }
