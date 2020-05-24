2eb80fc64457fcde46b6c469122762617f45206b
kamiazya <yuki@kamiazya.tech>
2020-05-24 18:55:47 +0900

    rollup d.ts files

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,9 @@
 import { rollup } from 'rollup';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
 import { remove } from 'fs-extra';
+import glob from 'glob';
 
 async function build({ input, output }: { input: string; output: string }): Promise<void> {
   const bundle = await rollup({
@@ -26,10 +28,45 @@ async function build({ input, output }: { input: string; output: string }): Prom
   ]);
 }
 
+async function rollupDts({
+  input,
+  inputDir,
+  output,
+}: {
+  input: string;
+  inputDir: string;
+  output: string;
+}): Promise<void> {
+  const bundle = await rollup({
+    input: `${inputDir}/${input}`,
+    plugins: [dts()],
+  });
+  const matches = await new Promise<string[]>((resolve, reject) => {
+    glob(`${inputDir}/**/*.d.ts`, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+  await Promise.all(matches.map((match) => remove(match)));
+
+  await bundle.write({
+    format: 'es',
+    file: `${output}/index.d.ts`,
+  });
+}
+
 (async (): Promise<void> => {
   await remove('lib');
   await build({
     input: 'src/index.ts',
+    output: 'lib',
+  });
+  await rollupDts({
+    inputDir: 'lib',
+    input: 'index.d.ts',
     output: 'lib',
   });
 })();
