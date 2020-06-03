6fd4a2a1e0fd5927a37b5c4b3f324b5594f96743
kamiazya <yuki@kamiazya.tech>
2020-06-03 23:17:45 +0900

    initial commit

---
+++ packages/node/rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      format: 'cjs',
+      file: './lib/index.js',
+    },
+    {
+      format: 'esm',
+      file: './lib/index.mjs',
+    },
+  ],
+  plugins: [typescript()],
+  external: ['ts-graphviz', 'child_process'],
+};
---
+++ packages/node/src/renderDot.ts
@@ -0,0 +1,54 @@
+import cp from 'child_process';
+import { IRootCluster, toDot } from 'ts-graphviz';
+
+/**
+ * Run dot command and output result to the specified path.
+ *
+ * ```ts
+ * import path from "path";
+ * import { digraph, attribute, renderDot } from "ts-graphviz";
+ *
+ * const G = digraph("G", (g) => {
+ *   const a = g.node("aa");
+ *   const b = g.node("bb");
+ *   const c = g.node("cc");
+ *   g.edge([a, b, c], {
+ *     [attribute.color]: "red",
+ *   });
+ *   g.subgraph("A", (A) => {
+ *     const Aa = A.node("Aaa", {
+ *       [attribute.color]: "pink",
+ *     });
+ *     const Ab = A.node("Abb", {
+ *       [attribute.color]: "violet",
+ *     });
+ *     const Ac = A.node("Acc");
+ *     A.edge([Aa.port({ compass: "c" }), Ab, Ac, "E"], {
+ *       [attribute.color]: "red",
+ *     });
+ *   });
+ * });
+ *
+ * renderDot(G, path.resolve(__dirname, "./callback.svg"), {
+ *   format: "svg",
+ * });
+ * ```
+ */
+export function renderDot(
+  dot: IRootCluster | string,
+  output: string,
+  {
+    format = 'png',
+    dotCommand = 'dot',
+  }: {
+    format?: 'png' | 'svg' | 'json' | 'jpg' | 'pdf' | 'xdot';
+    dotCommand?: string;
+  } = {},
+): void {
+  const input = typeof dot === 'string' ? dot : toDot(dot);
+  const cmd = [dotCommand, `-T${format}`, '-o', output];
+  cp.execSync(cmd.join(' '), {
+    stdio: 'pipe',
+    input,
+  });
+}
