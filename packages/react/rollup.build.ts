8026337cbd2d5ea01f4773709b90f652fb9afc79
Yuki Yamazaki <35218186+kamiazya@users.noreply.github.com>
2020-07-31 20:29:09 +0900

    Merge pull request #161 from ts-graphviz/rollup-dts
    rollup .d.ts and provides .min.js

--- packages/react/rollup.build.ts
+++ packages/react/rollup.build.ts
@@ -2,7 +2,9 @@ import { rollup } from 'rollup';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-import { remove } from 'fs-extra';
+import dts from 'rollup-plugin-dts';
+import { remove, removeSync } from 'fs-extra';
+import glob from 'glob';
 
 async function build({
   input,
@@ -27,7 +29,6 @@ async function build({
           },
         },
       }),
-      terser(),
     ],
     external,
   });
@@ -37,8 +38,35 @@ async function build({
       file: `${output}/index.js`,
     }),
     bundle.write({
+      format: 'cjs',
+      file: `${output}/index.min.js`,
+      plugins: [terser()],
+    }),
+    bundle.write({
       format: 'esm',
       file: `${output}/index.mjs`,
+    }),
+    bundle.write({
+      format: 'esm',
+      file: `${output}/index.min.mjs`,
+      plugins: [terser()],
+    }),
+  ]);
+}
+
+async function rollupDts({ output, external }: { output: string; external: string[] }): Promise<void> {
+  const bundle = await rollup({
+    input: `${output}/index.d.ts`,
+    plugins: [dts()],
+    external,
+  });
+
+  glob.sync(`${output}/**/*.d.ts`).forEach((file) => removeSync(file));
+
+  await Promise.all([
+    bundle.write({
+      format: 'es',
+      file: `${output}/index.d.ts`,
     }),
   ]);
 }
@@ -49,6 +77,10 @@ async function build({
     input: 'src/index.ts',
     output: 'lib',
     declaration: true,
-    external: ['react', 'react-dom/server', 'ts-graphviz', 'prop-types', 'react-reconciler', '@hpcc-js/wasm'],
+    external: ['react', 'react-dom/server', 'ts-graphviz', 'prop-types', 'react-reconciler'],
+  });
+  await rollupDts({
+    output: 'lib',
+    external: ['react', 'react-dom/server', 'ts-graphviz', 'prop-types', 'react-reconciler'],
   });
 })();
