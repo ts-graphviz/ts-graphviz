bf3b688f1d2ea8b930cb87493dfbb1558ca27b85
Yuki Yamazaki <35218186+kamiazya@users.noreply.github.com>
2022-09-23 07:06:09 +0000

    doc: update toDot doc

--- src/core/to-dot.ts
+++ src/core/to-dot.ts
@@ -1,7 +1,25 @@
 import { DotObjectModel } from '#lib/common';
-import { stringify, fromModel } from '#lib/ast';
+import { stringify, fromModel, ConvertFromModelOptions, PrintOptions } from '#lib/ast';
 
-export function toDot(model: DotObjectModel): string {
-  const ast = fromModel(model);
-  return stringify(ast);
+/**
+ * @group Convert Model to DOT
+ * @alpha
+ */
+export interface ToDotOptions {
+  convert?: ConvertFromModelOptions;
+  print?: PrintOptions;
+}
+
+/**
+ * Convert Model to DOT string.
+ *
+ * @group Convert Model to DOT
+ *
+ * @param model Dot Object Model, like {@link Digraph}, {@link Graph}, {@link Node}, and {@link Edge}
+ * @param options
+ * @returns DOT string
+ */
+export function toDot(model: DotObjectModel, options?: ToDotOptions): string {
+  const ast = fromModel(model, options?.convert);
+  return stringify(ast, options?.print);
 }
