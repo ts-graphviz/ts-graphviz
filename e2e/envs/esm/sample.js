e45eac3fd649fc922a9a17aea9950153374acf81
kamiazya <yuki@kamiazya.tech>
2022-12-07 14:37:40 +0000

    test: add e2e testing

---
+++ e2e/envs/esm/sample.js
@@ -0,0 +1,19 @@
+import { attribute as _, Digraph, Subgraph, Node, Edge, toDot } from 'ts-graphviz';
+
+const G = new Digraph();
+const A = new Subgraph('A');
+const node1 = new Node('node1', {
+  [_.color]: 'red',
+});
+const node2 = new Node('node2', {
+  [_.color]: 'blue',
+});
+const edge = new Edge([node1, node2], {
+  [_.label]: 'Edge Label',
+  [_.color]: 'pink',
+});
+G.addSubgraph(A);
+A.addNode(node1);
+A.addNode(node2);
+A.addEdge(edge);
+toDot(G);
