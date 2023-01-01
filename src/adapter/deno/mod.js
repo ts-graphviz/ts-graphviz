ab0811a316a9a11415bced6f22a0c4e1bd247675
Yuki Yamazaki <35218186+kamiazya@users.noreply.github.com>
2023-01-01 22:34:38 +0900

    fix: fix for deno e2e test failed (#753)
    * fix: fix for deno e2e test failed
    
    * fix: handle file extsts

--- src/adapter/deno/mod.js
+++ src/adapter/deno/mod.js
@@ -16,6 +16,7 @@ export async function toStream(dot, options) {
   const cp = new Deno.Command(command, {
     args: args,
     stdin: 'piped',
+    stdout: 'piped',
   }).spawn();
   const stdin = cp.stdin.getWriter();
   await stdin.write(new TextEncoder().encode(dot));
@@ -23,11 +24,22 @@ export async function toStream(dot, options) {
   return cp.stdout;
 }
 
+function open(path) {
+  try {
+    return Deno.open(path, { write: true });
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      return Deno.open(path, { createNew: true, write: true });
+    }
+    throw e;
+  }
+}
+
 /**
  * Execute the Graphviz dot command and output the results to a file.
  */
 export async function toFile(dot, path, options) {
-  const output = await Deno.open(path, { write: true });
+  const output = await open(path);
   const stream = await toStream(dot, options);
   await stream.pipeTo(output.writable);
 }
