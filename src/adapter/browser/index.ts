b80ee87d9464a62d9956686236cfa70ed1e6ef7b
Yuki Yamazaki <35218186+kamiazya@users.noreply.github.com>
2023-01-07 00:16:02 +0900

    feat: Support webpack build (#755)
    * fix: fix conditional export and add webpack e2e test
    
    * chaos: fix typo
    
    * chaos: add webpack web build e2e test
    
    * chaos: remove useless cat in e2e scripts
    
    * refactor: fix erports

---
+++ src/adapter/browser/index.ts
@@ -0,0 +1,22 @@
+/**
+ * @module ts-graphviz/adapter
+ * @beta
+ */
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
+
+export type Options = any;
+
+const ERROR_MESSAGE = 'This module cannot be run in a browser.';
+/**
+ * Execute the Graphviz dot command and make a Stream of the results.
+ */
+export function toStream(dot: string, options?: Options): never {
+  throw new Error(ERROR_MESSAGE);
+}
+
+/**
+ * Execute the Graphviz dot command and output the results to a file.
+ */
+export function toFile(dot: string, path: string, options?: Options): never {
+  throw new Error(ERROR_MESSAGE);
+}
