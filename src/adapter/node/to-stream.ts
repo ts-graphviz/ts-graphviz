d42db466fd860569e2068fd5d28def85c52f0f17
Yuki Yamazaki <35218186+kamiazya@users.noreply.github.com>
2023-01-17 13:49:24 +0900

    feat: add error handling for adapter module (#778)

--- src/adapter/node/to-stream.ts
+++ src/adapter/node/to-stream.ts
@@ -1,4 +1,4 @@
-import { Readable } from 'node:stream';
+import { Readable, PassThrough } from 'node:stream';
 import { spawn } from 'node:child_process';
 import { Layout, Options } from '../types/index.js';
 import { commandBuilder } from '../utils/index.js';
@@ -9,7 +9,31 @@ import { pipeline } from './utils.js';
  */
 export async function toStream<T extends Layout>(dot: string, options?: Options<T>): Promise<NodeJS.ReadableStream> {
   const [command, args] = commandBuilder(options ?? {});
-  const p = spawn(command, args, { stdio: 'pipe' });
-  await pipeline(Readable.from([dot]), p.stdin);
-  return p.stdout;
+  return new Promise(async function toStreamInternal(resolve, reject) {
+    const p = spawn(command, args, { stdio: 'pipe' });
+
+    // error handling
+    p.on('error', (e) => {
+      reject(
+        new Error(`Command "${command}" failed.\nMESSAGE:${e.message}`, {
+          cause: e,
+        }),
+      );
+    });
+
+    const stderrChunks: Uint8Array[] = [];
+    p.stderr.on('data', (chunk) => stderrChunks.push(chunk));
+
+    const dist = p.stdout.pipe(new PassThrough());
+    p.on('close', async (code, signal) => {
+      if (code === 0) {
+        resolve(dist);
+      } else {
+        const message = Buffer.concat(stderrChunks as ReadonlyArray<Uint8Array>).toString();
+        reject(new Error(`Command "${command}" failed.\nCODE: ${code}\nSIGNAL: ${signal}\nMESSAGE: ${message}`));
+      }
+    });
+
+    await pipeline(Readable.from([dot]), p.stdin);
+  });
 }
