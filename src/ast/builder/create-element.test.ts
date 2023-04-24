cb400eca72c38dca88b524c5bc720f5c0a0396d3
kamiazya <yuki@kamiazya.tech>
2023-04-24 09:24:12 +0000

    fix: fix AST Builer

--- src/ast/builder/create-element.test.ts
+++ src/ast/builder/create-element.test.ts
@@ -7,123 +7,67 @@ describe('createElement', () => {
   });
 
   test('Graph', () => {
-    const ast = createElement(
-      'Graph',
-      {
-        strict: false,
-        directed: true,
-      },
-      [],
-    );
+    const ast = createElement('Graph', {
+      strict: false,
+      directed: true,
+    });
     expect(ast).toMatchSnapshot();
   });
 
   test('Node', () => {
-    const ast = createElement(
-      'Node',
-      {
-        id: createElement(
-          'Literal',
-          {
-            value: 'node_id',
-            quoted: false,
-          },
-          [],
-        ),
-      },
-      [],
-    );
+    const ast = createElement('Node', {
+      id: createElement('Literal', {
+        value: 'node_id',
+        quoted: false,
+      }),
+    });
     expect(ast).toMatchSnapshot();
   });
 
   test('Edge', () => {
-    const ast = createElement(
-      'Edge',
-      {
-        targets: [
-          createElement(
-            'NodeRef',
-            {
-              id: createElement(
-                'Literal',
-                {
-                  value: 'a',
-                  quoted: true,
-                },
-                [],
-              ),
-            },
-            [],
-          ),
-          createElement(
-            'NodeRef',
-            {
-              id: createElement(
-                'Literal',
-                {
-                  value: 'b',
-                  quoted: true,
-                },
-                [],
-              ),
-            },
-            [],
-          ),
-        ],
-      },
-      [],
-    );
+    const ast = createElement('Edge', {
+      targets: [
+        createElement('NodeRef', {
+          id: createElement('Literal', {
+            value: 'a',
+            quoted: true,
+          }),
+        }),
+        createElement('NodeRef', {
+          id: createElement('Literal', {
+            value: 'b',
+            quoted: true,
+          }),
+        }),
+      ],
+    });
     expect(ast).toMatchSnapshot();
   });
 
   test('NodeRef', () => {
-    const ast = createElement(
-      'NodeRef',
-      {
-        id: createElement(
-          'Literal',
-          {
-            value: 'b',
-            quoted: true,
-          },
-          [],
-        ),
-      },
-      [],
-    );
+    const ast = createElement('NodeRef', {
+      id: createElement('Literal', {
+        value: 'b',
+        quoted: true,
+      }),
+    });
     expect(ast).toMatchSnapshot();
   });
 
   test('NodeRefGroup', () => {
     const ast = createElement('NodeRefGroup', {}, [
-      createElement(
-        'NodeRef',
-        {
-          id: createElement(
-            'Literal',
-            {
-              value: 'a',
-              quoted: true,
-            },
-            [],
-          ),
-        },
-        [],
-      ),
-      createElement(
-        'NodeRef',
-        {
-          id: createElement(
-            'Literal',
-            {
-              value: 'b',
-              quoted: true,
-            },
-            [],
-          ),
-        },
-        [],
-      ),
+      createElement('NodeRef', {
+        id: createElement('Literal', {
+          value: 'a',
+          quoted: true,
+        }),
+      }),
+      createElement('NodeRef', {
+        id: createElement('Literal', {
+          value: 'b',
+          quoted: true,
+        }),
+      }),
     ]);
     expect(ast).toMatchSnapshot();
   });
@@ -134,52 +78,32 @@ describe('createElement', () => {
   });
 
   test('Literal', () => {
-    const ast = createElement(
-      'Literal',
-      {
-        value: 'literal value',
-        quoted: true,
-      },
-      [],
-    );
+    const ast = createElement('Literal', {
+      value: 'literal value',
+      quoted: true,
+    });
     expect(ast).toMatchSnapshot();
   });
 
   test('Coment', () => {
-    const ast = createElement(
-      'Comment',
-      {
-        kind: 'Macro',
-        value: 'This is comment',
-      },
-      [],
-    );
+    const ast = createElement('Comment', {
+      kind: 'Macro',
+      value: 'This is comment',
+    });
     expect(ast).toMatchSnapshot();
   });
 
   test('Attribute', () => {
-    const ast = createElement(
-      'Attribute',
-      {
-        key: createElement(
-          'Literal',
-          {
-            value: 'color',
-            quoted: false,
-          },
-          [],
-        ),
-        value: createElement(
-          'Literal',
-          {
-            value: 'red',
-            quoted: false,
-          },
-          [],
-        ),
-      },
-      [],
-    );
+    const ast = createElement('Attribute', {
+      key: createElement('Literal', {
+        value: 'color',
+        quoted: false,
+      }),
+      value: createElement('Literal', {
+        value: 'red',
+        quoted: false,
+      }),
+    });
     expect(ast).toMatchSnapshot();
   });
 });
