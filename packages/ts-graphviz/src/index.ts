c8e3aeeeb035003e5f47751ee794d234c648f9e4
kamiazya <yuki@kamiazya.tech>
2023-05-20 14:51:51 +0000

    refactor: Move Utility APIs, toDot/fromDot API to ts-graphviz from @ts-graphviz/core

--- packages/ts-graphviz/src/index.ts
+++ packages/ts-graphviz/src/index.ts
@@ -5,3 +5,5 @@ export * from '@ts-graphviz/common';
 export * from '@ts-graphviz/core';
 export * from './model-factory/index.js';
 export * from './attribute.js';
+export * from './to-dot.js';
+export * from './from-dot.js';
