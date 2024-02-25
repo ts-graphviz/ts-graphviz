8eb835a99bff10d77886e4426b918c8fdb725d5e
kamiazya <yuki@kamiazya.tech>
2024-02-25 13:54:54 +0000

    Remove deprecated AttributeKeyDict type

--- packages/ts-graphviz/src/attribute.ts
+++ packages/ts-graphviz/src/attribute.ts
@@ -2,13 +2,6 @@ import { Attribute } from '@ts-graphviz/common';
 
 /**
  * @group Attribute
- *
- * @deprecated Use {@link Attribute.keys} instead.
- */
-export type AttributeKeyDict = Attribute.keys;
-
-/**
- * @group Attribute
  */
 export const attribute: Attribute.keys = new Proxy(
   Object.freeze({}) as Attribute.keys,
