a33b84c350ead60070885c8a98667e973da96938
kamiazya <yuki@kamiazya.tech>
2024-03-15 15:03:27 +0000

    Update biome.json and errors.ts

--- packages/react/src/errors.ts
+++ packages/react/src/errors.ts
@@ -1,7 +1,11 @@
-export const EdgeTargetLengthErrorMessage = 'Edges must have at least 2 targets.';
+export const EdgeTargetLengthErrorMessage =
+  'Edges must have at least 2 targets.';
 export const NoGraphvizContextErrorMessage =
   'Cannot call useGraphvizContext outside GraphvizContext.\nBasically, you need to use the render function provided by @ts-graphviz/react.';
-export const NoClusterErrorMessage = 'useCluster must be called within a cluster such as Digraph, Graph, Subgraph.';
-export const DuplicatedRootClusterErrorMessage = 'RootCluster is duplicated.\nUse only one of Digraph and Graph.';
+export const NoClusterErrorMessage =
+  'useCluster must be called within a cluster such as Digraph, Graph, Subgraph.';
+export const DuplicatedRootClusterErrorMessage =
+  'RootCluster is duplicated.\nUse only one of Digraph and Graph.';
 
-export const NoContainerErrorMessage = 'There are no clusters of container(Subgraph, Digraph, Graph).';
+export const NoContainerErrorMessage =
+  'There are no clusters of container(Subgraph, Digraph, Graph).';
