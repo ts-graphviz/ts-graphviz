87fe837183be017d7cea9801a91d93b94d821ba4
kamiazya <yuki@kamiazya.tech>
2024-09-26 23:57:16 +0900

    Improve string escaping in DOT language

--- packages/ast/src/dot-shim/printer/plugins/utils/tokens.test.ts
+++ packages/ast/src/dot-shim/printer/plugins/utils/tokens.test.ts
@@ -10,4 +10,20 @@ describe('escape', () => {
   ])('should escape %s to %s', (value, expected) => {
     expect(escape(value)).toBe(expected);
   });
+
+  test('should escape multiple characters', () => {
+    expect(escape('\r\n"')).toBe(String.raw`\r\n\"`);
+  });
+
+  test('should not escape other characters', () => {
+    expect(escape('abc')).toBe('abc');
+  });
+
+  test('should not escape escaped characters', () => {
+    expect(escape(String.raw`\r\n\"`)).toBe(String.raw`\r\n\"`);
+  });
+
+  test('should not escape empty string', () => {
+    expect(escape('')).toBe('');
+  });
 });
--- packages/ast/src/dot-shim/printer/plugins/utils/tokens.ts
+++ packages/ast/src/dot-shim/printer/plugins/utils/tokens.ts
@@ -26,16 +26,22 @@ export function rightPadWith(right: string): (value: string) => string {
 /**
  * Escape a string for literal value in DOT language.
  *
+ * @remarks
  * The following characters are escaped:
- * - `\r` -> `\r`
- * - `\n` -> `\n`
- * - `"` -> `\"`
+ * - `\r` -> String.raw`\r`
+ * - `\n` -> String.raw`\n`
+ * - `"` -> String.raw`\"`
+ *
+ * But escaped characters are not escaped again.
  *
  * @param value - The string to escape
  * @returns The escaped string
  */
 export const escape = (value: string): string =>
-  value.replace(/[\r\n"]/g, escapeReplacer);
+  value.replace(/(?<!\\)"|[\r\n]/g, escapeReplacer);
+// NOTE: The regular expression used to escape the string is `/(?<!\\)"|[\r\n]/g`.
+// - `(?<!\\)"`: This part of the regular expression matches a double quote (`"`) **only if** it is **not preceded by a backslash**, effectively ignoring already-escaped quotes like `\"`.
+// - `[\r\n]`: This part matches every carriage return (`\r`) or newline (`\n`) character in the input, regardless of whether they are escaped.
 function escapeReplacer(match: string) {
   switch (match) {
     case '\r':
