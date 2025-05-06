49c3f52f2920aa038c01223b6b01dd8f315431ec
kamiazya <yuki@kamiazya.tech>
2025-05-06 17:22:15 +0900

    chore: Biome to 1.9.4 version

--- packages/react/src/hooks/useGraph.ts
+++ packages/react/src/hooks/useGraph.ts
@@ -21,6 +21,7 @@ export function useGraph(options: RootGraphOptions = {}): RootGraphModel {
     g.attributes.edge.apply(edge ?? {});
     g.attributes.graph.apply(graph ?? {});
     return g;
+    // biome-ignore lint/correctness/useExhaustiveDependencies: FIXME attributes changes on every re-render and should not be used as a hook dependency.
   }, [context, id, comment, edge, node, graph, attributes]);
   useHasComment(memoGraph, comment);
   useGraphAttributes(memoGraph, attributes, { edge, node, graph });
