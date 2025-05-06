f42d5401f871c55dffc7ef45001ec4fbe8095ede
Yuki Yamazaki <yuki@kamiazya.tech>
2025-05-06 11:28:31 +0900

    Adjust HTML label AST handling for consistent behavior #1335

---
+++ .changeset/slimy-geckos-nail.md
@@ -0,0 +1,6 @@
+---
+"@ts-graphviz/ast": major
+"@ts-graphviz/react": minor
+---
+
+Adjust HTML label AST handling for consistent behavior #1335
---
+++ packages/ast/src/model-shim/from-model/plugins/utils/convert-attribute.test.ts
@@ -0,0 +1,83 @@
+import { describe, expect, test } from 'vitest';
+import { convertAttribute } from './convert-attribute.js';
+
+describe('convertAttribute', () => {
+  test('number', () => {
+    const result = convertAttribute('size', 1);
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "children": [],
+        "key": {
+          "children": [],
+          "location": null,
+          "quoted": false,
+          "type": "Literal",
+          "value": "size",
+        },
+        "location": null,
+        "type": "Attribute",
+        "value": {
+          "children": [],
+          "location": null,
+          "quoted": false,
+          "type": "Literal",
+          "value": "1",
+        },
+      }
+    `);
+  });
+
+  describe('string', () => {
+    test('quated', () => {
+      const result = convertAttribute('label', 'A');
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "children": [],
+          "key": {
+            "children": [],
+            "location": null,
+            "quoted": false,
+            "type": "Literal",
+            "value": "label",
+          },
+          "location": null,
+          "type": "Attribute",
+          "value": {
+            "children": [],
+            "location": null,
+            "quoted": true,
+            "type": "Literal",
+            "value": "A",
+          },
+        }
+      `);
+    });
+
+    describe('html-like', () => {
+      test('html', () => {
+        const result = convertAttribute('label', '<A>');
+        expect(result).toMatchInlineSnapshot(`
+          {
+            "children": [],
+            "key": {
+              "children": [],
+              "location": null,
+              "quoted": false,
+              "type": "Literal",
+              "value": "label",
+            },
+            "location": null,
+            "type": "Attribute",
+            "value": {
+              "children": [],
+              "location": null,
+              "quoted": "html",
+              "type": "Literal",
+              "value": "<A>",
+            },
+          }
+        `);
+      });
+    });
+  });
+});
